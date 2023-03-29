629da971a50f40d702e922385b91307e69c365f6
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2023-03-29 21:40:21 +0900

    feat(game_screen): game screen 틀 작성

---
+++ lib/screen/game_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({Key? key}) : super(key: key);
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  int size = 0;
+  int winningCondition = 0;
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
