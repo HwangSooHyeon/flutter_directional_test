7dae64ca96d0d358ba0ed0dd82da6a4f9e81a976
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2023-03-29 21:29:16 +0900

    feat(home_screen): home screen ui 틀 작성

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -5,6 +5,30 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        padding: EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                '게임 시작',
+              ),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                '저장된 게임',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
