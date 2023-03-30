2f817dbb522fc5a46323afd8eac9d898f69ca3b6
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2023-03-30 18:51:25 +0900

    feat(player): player 모델에 turn, returnCount를 수정하는 메서드 추가

--- lib/model/player.dart
+++ lib/model/player.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 class Player {
   final String name;
-  final bool isTurn;
   final IconData iconData;
+  bool isTurn;
   int returnCount = 3;
 
   Player({
@@ -11,6 +11,14 @@ class Player {
     required this.isTurn,
     required this.iconData,
   });
+
+  void setTurn() {
+    isTurn ? isTurn = false : isTurn = true;
+  }
+
+  void decReturnCount() {
+    if (returnCount >= 0) returnCount--;
+  }
 
   @override
   String toString() {
