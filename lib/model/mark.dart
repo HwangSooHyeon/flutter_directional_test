e28fd9068650076ff0f74116cffb84c4e4b5d633
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2023-03-30 21:54:31 +0900

    feat(mark): mark 모델에 index 저장 필드 추가

--- lib/model/mark.dart
+++ lib/model/mark.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 class Mark {
   final IconData iconData;
   final int turn;
+  final int index;
 
   Mark({
     required this.iconData,
     required this.turn,
+    required this.index,
   });
 }
