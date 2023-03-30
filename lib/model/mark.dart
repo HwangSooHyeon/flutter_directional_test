08141a0bea7dc04ca607d83666fbdd39c7a1689f
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2023-03-31 00:53:38 +0900

    feat(mark): mark 모델에 playerName 필드 추가

--- lib/model/mark.dart
+++ lib/model/mark.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 class Mark {
+  final String playerName;
   final IconData iconData;
   final int turn;
   final int index;
 
   Mark({
+    required this.playerName,
     required this.iconData,
     required this.turn,
     required this.index,
