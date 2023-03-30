a027aaa8554486c2432d147a2a32be1ac1d7e152
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2023-03-31 00:54:08 +0900

    feat(game_board): 승자 결정 메서드 작성

--- lib/model/game_board.dart
+++ lib/model/game_board.dart
@@ -37,6 +37,89 @@ class GameBoard {
     }
   }
 
+  String checkEnd() {
+    int count = 0;
+    if (history.length == size * size) {
+      return '무승부';
+    }
+    int col = history.last.index ~/ size;
+    int row = history.last.index % size;
+    String playerName = history.last.playerName;
+    // 가로 체크
+    while (progress[col][row] != null && progress[col][row]!.playerName == playerName && row > 0) {
+      row--;
+    }
+    while (progress[col][row] != null && progress[col][row]!.playerName == playerName && row < size) {
+      count++;
+      row++;
+      if (row == size) {
+        break;
+      }
+    }
+    if (count == winningCondition) {
+      return playerName;
+    }
+    // 세로 체크
+    col = history.last.index ~/ size;
+    row = history.last.index % size;
+    count = 0;
+    while (progress[col][row] != null && progress[col][row]!.playerName == playerName && col > 0) {
+      col--;
+    }
+    while (progress[col][row] != null && progress[col][row]!.playerName == playerName && col < size) {
+      count++;
+      col++;
+      if (col == size) {
+        break;
+      }
+    }
+    if (count == winningCondition) {
+      return playerName;
+    }
+    // 좌상-우하 대각선 체크
+    col = history.last.index ~/ size;
+    row = history.last.index % size;
+    count = 0;
+    while (progress[col][row] != null && progress[col][row]!.playerName == playerName && col > 0 && row > 0) {
+      col--;
+      row--;
+    }
+    while (progress[col][row] != null && progress[col][row]!.playerName == playerName && col < size && row < size) {
+      count++;
+      col++;
+      row++;
+      if(col == size || row == size) {
+        break;
+      }
+    }
+    if (count == winningCondition) {
+      return playerName;
+    }
+    // 좌하-우상 대각선 체크
+    col = history.last.index ~/ size;
+    row = history.last.index % size;
+    count = 0;
+    while (progress[col][row] != null && progress[col][row]!.playerName == playerName && col < size - 1 && row > 0) {
+      col++;
+      row--;
+      if (col == size - 1 || row == 0) {
+        break;
+      }
+    }
+    while (progress[col][row] != null && progress[col][row]!.playerName == playerName && col >= 0 && row <= size - 1) {
+      count++;
+      col--;
+      row++;
+      if (col == -1 || row == size) {
+        break;
+      }
+    }
+    if (count == winningCondition) {
+      return playerName;
+    }
+    return '진행';
+  }
+
   @override
   String toString() {
     return 'size: $size, winningCondition: $winningCondition';
