d6af8b86a72cf096ab66ebaa59a6ba2f905f30ab
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2023-03-31 23:55:25 +0900

    feat(custom_push_button): push 전용 버튼 구현

---
+++ lib/component/custom_push_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CustomPushButton extends StatelessWidget {
+  final Widget screen;
+  final String label;
+
+  const CustomPushButton({
+    Key? key,
+    required this.screen,
+    required this.label,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (_) => screen),
+        );
+      },
+      child: const Text(
+        '게임 시작',
+      ),
+    );
+  }
+}
